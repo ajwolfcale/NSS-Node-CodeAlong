02a05194bf80b0508e60db2eccc377ea64528ba4
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-03-14 10:46:05 -0500

    readme update

--- express101/app.js
+++ express101/app.js
@@ -5,6 +5,7 @@ const app = express();
 console.log("Hello from Express");
 
 
-app.listen(8080, () => {
-    console.log('listening on port 8080');
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
